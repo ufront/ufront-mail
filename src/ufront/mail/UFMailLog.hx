8e5566a3ce535e50b2110595af30599d61c1e318
Jason O'Neil <jason.oneil@gmail.com>
2014-09-18 13:55:19 +0800

    Split out UFMailLog into own file, create DBMailerAdminModule

---
+++ src/ufront/mail/UFMailLog.hx
@@ -0,0 +1,13 @@
+package ufront.mail;
+
+import ufront.db.Object;
+import sys.db.Types;
+
+class UFMailLog extends Object {
+	public var to:SString<255>;
+	public var from:SString<255>;
+	public var subject:SString<255>;
+	public var date:SDateTime;
+	public var email:SData<Email>;
+	public var numAttachments:Int;
+}
